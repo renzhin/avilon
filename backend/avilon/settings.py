60adb1a075af33d0021f74f5f0d371f261efbb5c
Михаил Ренжин <renzhin@gmail.com>
2024-06-25 08:40:26 +0300

    Исправить ошибку LANGUAGE_CODE в settings.py

--- backend/avilon/settings.py
+++ backend/avilon/settings.py
@@ -190,7 +190,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'ru-RU'
+LANGUAGE_CODE = 'ru'
 
 LANGUAGES = [
     ("en", _("English")),
